499dce2cddf177218200fc53667fd2c992109301
Gustavo de Oliveira <gustavooliveira@sicredi.com.br>
2024-01-28 21:18:44 -0300

    refactor(hook): hook updated to install in the actual dir

--- setup-hooks.sh
+++ setup-hooks.sh
@@ -1,18 +1,7 @@
 #!/bin/sh
 
-if [ "$#" -ne 1 ]; then
-    echo "ERRO: Caminho para o repositorio git de destino não informado. Exemplo de uso correto do script: setup-hooks.sh <caminho-para-repositorio-git>"
-    exit 1
-else
-    REPO_PATH=$1
-    echo "Configurando pre-commit hook no repositório: $REPO_PATH"
-fi
-
-
-if [ ! -d "$REPO_PATH/.git" ]; then
-    echo "Erro: O caminho especificado não parece ser um repositório Git válido: $REPO_PATH"
-    exit 1
-fi
+REPO_PATH=$1
+echo "Configurando pre-commit hook no repositório: $REPO_PATH"
 
 cp pre-commit "$REPO_PATH/.git/hooks/pre-commit"
 chmod +x "$REPO_PATH/.git/hooks/pre-commit"
